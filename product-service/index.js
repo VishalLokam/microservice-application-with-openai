383abf18fd87e4b2f43efbce66d4bc83c8efc040
VishalLokam <vishalvikaslokam@gmail.com>
2024-02-16 21:48:47 +0530

    Fix mongodb connection

--- product-service/index.js
+++ product-service/index.js
@@ -3,10 +3,11 @@ const app = express();
 const mongoose  = require("mongoose")
 const PORT = process.env.PORT_ONE || 8080
 const jwt = require("jsonwebtoken")
+require('dotenv').config()
 
 app.use(express.json())
 
-mongoose.connect("product-service?retryWrites=true&w=majority").then(
+mongoose.connect(`${process.env.CONNECTION_STRING}product-service?retryWrites=true&w=majority`).then(
     () => { console.log("Product-Service DB connected")},
     err => { handleError(err)  }
 )
@@ -14,5 +15,5 @@ mongoose.connect("product-service?retryWrites=true&w=majority").then(
 
 
 app.listen(PORT, () => {
-    console.log(`Auth-service at ${PORT}`)
+    console.log(`Product-service at ${PORT}`)
 })
