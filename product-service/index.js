f64bef859d1e77ddeceacde10a704dce0e6774b2
VishalLokam <vishalvikaslokam@gmail.com>
2024-03-04 00:11:46 +0530

    Added functionality to add products

--- product-service/index.js
+++ product-service/index.js
@@ -3,7 +3,12 @@ const app = express();
 const mongoose  = require("mongoose")
 const PORT = process.env.PORT_ONE || 8080
 const jwt = require("jsonwebtoken")
+var amqp = require("amqplib/callback_api")
+const Product = require("./Product")
+const isAuthenticated = require("../isAuthenticated")
 require('dotenv').config()
+
+var channel, connection
 
 app.use(express.json())
 
@@ -12,7 +17,38 @@ mongoose.connect(`${process.env.CONNECTION_STRING}product-service?retryWrites=tr
     err => { handleError(err)  }
 )
 
+amqp.connect('amqp://localhost', function(error0, connection) {
+  if (error0) {
+    throw error0;
+  }
+  connection.createChannel(function(error1, channel) {
+    if (error1) {
+      throw error1;
+    }
 
+    channel.assertQueue("PRODUCT");
+
+    // channel.sendToQueue(queue, Buffer.from(msg));
+    // console.log(" [x] Sent %s", msg);
+  });
+});
+
+
+//Create a new product
+app.post("/product/create",isAuthenticated, async(req, res) => {
+    const {name, tags, description, price} = req.body
+    const newProduct = new Product({
+        name,
+        tags,
+        description,
+        price
+    })
+    newProduct.save()
+    return res.json(newProduct)
+})
+
+
+//Buy a product
 
 app.listen(PORT, () => {
     console.log(`Product-service at ${PORT}`)
